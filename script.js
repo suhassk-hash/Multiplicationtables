bf02a52a276999d014cd3613911d40ae8cfcabea
Suhas Sharma Koheda <sharmasuhas450@gmail.com>
2023-06-02 12:20:20 +0530

    hi

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function main() {
-    var n = parseInt(document.getElementById("inputN").value);
-    var m = parseInt(document.getElementById("inputM").value);
-    
-    var output = document.getElementById("output");
-    output.innerHTML = "";
-    
-    for (var i = 1; i <= m; i++) {
-      var o = i * n;
-      output.innerHTML += n + "x" + i + "=" + o + "<br>";
-    }
-  }
-  
+function calculateTotalCost() {
+    var consumerName = document.getElementById("consumerName").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var units = parseFloat(document.getElementById("units").value);
+    var totalUnitCost = 0.0;
+    var totalCost = 0.0;
+
+    if (units > 0 && units <= 50)
+        totalUnitCost = 7.00 * units;
+    else if (units > 50 && units <= 100)
+        totalUnitCost = 50 * 7.00 + ((units - 50) * 8.50);
+    else if (units > 100 && units <= 300)
+        totalUnitCost = 50 * 7.00 + 50 * 8.50 + ((units - 100) * 9.90);
+    else if (units > 300 && units <= 500)
+        totalUnitCost = 50 * 7.00 + 50 * 8.50 + 200 * 9.90 + ((units - 300) * 10.40);
+    else if (units > 500)
+        totalUnitCost = 50 * 7.00 + 50 * 8.50 + 200 * 9.90 + 200 * 10.40 + ((units - 500) * 11.00);
+
+    totalCost = totalUnitCost + 65;
+
+    document.getElementById("output").innerHTML =
+        "<p>Consumer Name: " + consumerName + "</p>" +
+        "<p>Phone Number: " + phoneNumber + "</p>" +
+        "<p>Total Units: " + units + "</p>" +
+        "<p>Total Cost: " + totalCost + "</p>" +
+        "<p>Monthly minimum energy charges: Rs.65/month for single-phase supply</p>";
+}
